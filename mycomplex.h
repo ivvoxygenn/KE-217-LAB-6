b50b984f89b7fecd7340eb1262290a4184f054bf
Student 2 <student_2@aiu.local>
2024-12-06 08:44:44 +0500

    Исходники

--- mycomplex.h
+++ mycomplex.h
@@ -2,35 +2,38 @@
 #define _MY_COMPLEX_H_
 #include <iostream>
 using namespace std;
-class Complex {
-double Re;
-double Im;
-public:
+class Complex
+{double Re;
+	double Im; 
+	public:
 Complex(double aRe=0, double aIm=0);
 Complex(const Complex&);
-~Complex();
+~Complex(    );
 void Set(double aRe, double aIm=0);
 operator double();
-double abs();
-friend istream& operator>>(istream&,Complex&) ;
-friend ostream& operator<< (ostream&, Complex &);
+ double abs();
+ friend istream& operator>>(   istream&,Complex&) ; 
+
+
+
+friend ostream& operator<< (ostream&, Complex & );
+
 Complex operator+ (const Complex&);
 Complex operator- (const Complex&);
 Complex operator+ (const double&);
-friend Complex operator+ (const double&, const Complex&);
+friend 		Complex operator+ (const double&, const Complex&);
 Complex operator- (const double&);
-friend Complex operator- (const double&, const Complex&);
+friend Complex operator- (const 	double&, const Complex&);
 Complex operator* (const Complex&);
 Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&);
-Complex operator/ (const double&);
-Complex & operator+= (const Complex&);
-Complex & operator-= (const Complex&);
-Complex & operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex & operator-= (const double&);
-Complex & operator  *=(const double  & );
-Complex & operator /=( const   double & );
-Complex & operator = (  const   Complex&);
-Complex & operator= (const double&);
-}
+friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
+
+    Complex& operator+= (const Complex&);
+Complex  &  operator-= (const Complex&)    ;
+    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
+Complex& operator-= (const double&);
+Complex  &operator  *=(const double  & );
+Complex  & operator /=( const   double & );
+Complex& operator = (  const   Complex&);
+Complex & operator= (const double&);};
 #endif
