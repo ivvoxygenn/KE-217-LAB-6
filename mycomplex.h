877d113ca29580ec2855d81b7fc9e182425ad0f4
ivvoxygenn <87770488479this@gmail.com>
2024-12-06 08:50:54 +0500

    Приведено к общепринятому стандарту кодирования

--- mycomplex.h
+++ mycomplex.h
@@ -4,36 +4,34 @@
 using namespace std;
 class Complex
 {double Re;
-	double Im; 
-	public:
+double Im; 
+public:
 Complex(double aRe=0, double aIm=0);
 Complex(const Complex&);
-~Complex(    );
+~Complex();
 void Set(double aRe, double aIm=0);
 operator double();
- double abs();
- friend istream& operator>>(   istream&,Complex&) ; 
-
-
-
-friend ostream& operator<< (ostream&, Complex & );
-
+double abs();
+friend istream& operator>>(istream&,Complex&) ; 
+friend ostream& operator<< (ostream&, Complex&);
 Complex operator+ (const Complex&);
 Complex operator- (const Complex&);
 Complex operator+ (const double&);
-friend 		Complex operator+ (const double&, const Complex&);
+friend Complex operator+ (const double&, const Complex&);
 Complex operator- (const double&);
-friend Complex operator- (const 	double&, const Complex&);
+friend Complex operator- (const double&, const Complex&);
 Complex operator* (const Complex&);
 Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
-
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+friend Complex operator* (const double&, const Complex&);
+Complex operator/ (const double&);
+Complex & operator+= (const Complex&);
+Complex & operator-= (const Complex&);
+Complex & operator*=(const Complex& );
+Complex & operator+=(const double&);
+Complex & operator-=(const double&);
+Complex & operator*=(const double  & );
+Complex & operator/=( const double & );
+Complex & operator=(const Complex&);
+Complex & operator=(const double&);
+};
 #endif
